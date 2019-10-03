a7b519b2269a8fd4acd30ae3e586c84b86939212
Dmitrii Matveev <matveyeff@mail.ru>
2019-10-03 23:13:34 +0300

    added accordeon

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,22 @@ hamburger.addEventListener('click', function() {
 	style.display = 'flex';
 	hamburger.style.display = 'none';
 })
+
+var team = document.getElementsByClassName('accordeon__item');
+
+for (var i = 0; i < team.length; i++) {
+	team[i].addEventListener('click', function(e) {
+		e.preventDefault();
+		if (!(this.classList.contains('accordeon__item_active'))) {
+			for (var i = 0; i < team.length; i++) {
+				team[i].classList.remove('accordeon__item_active');
+			}
+			this.classList.add('accordeon__item_active');
+		}
+		else if (this.classList.contains('accordeon__item_active')) {
+			for (var i = 0; i < team.length; i++) {
+				team[i].classList.remove('accordeon__item_active');
+			}
+		}
+	})
+}
