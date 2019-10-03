bc484b3806a989c0c6737eb22cbda5cc2d52da12
Dmitrii Matveev <matveyeff@mail.ru>
2019-10-04 02:42:57 +0300

    added section Menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+// Hamburger Menu
+
 var hamburger = document.getElementById('mobile');
 var open = document.getElementById('nav');
 var style = open.style;
@@ -7,20 +9,43 @@ hamburger.addEventListener('click', function() {
 	hamburger.style.display = 'none';
 })
 
-var team = document.getElementsByClassName('accordeon__item');
+// Team Accordeon
 
-for (var i = 0; i < team.length; i++) {
-	team[i].addEventListener('click', function(e) {
+var accordeonItemTeam = document.getElementsByClassName('accordeon__item');
+
+for (var i = 0; i < accordeonItemTeam.length; i++) {
+	accordeonItemTeam[i].addEventListener('click', function(e) {
 		e.preventDefault();
 		if (!(this.classList.contains('accordeon__item_active'))) {
-			for (var i = 0; i < team.length; i++) {
-				team[i].classList.remove('accordeon__item_active');
+			for (var i = 0; i < accordeonItemTeam.length; i++) {
+				accordeonItemTeam[i].classList.remove('accordeon__item_active');
 			}
 			this.classList.add('accordeon__item_active');
 		}
 		else if (this.classList.contains('accordeon__item_active')) {
-			for (var i = 0; i < team.length; i++) {
-				team[i].classList.remove('accordeon__item_active');
+			for (var i = 0; i < accordeonItemTeam.length; i++) {
+				accordeonItemTeam[i].classList.remove('accordeon__item_active');
+			}
+		}
+	})
+}
+
+// Menu Accordeon
+
+var acc = document.getElementsByClassName('menu-acc__item');
+
+for (var i = 0; i < acc.length; i++) {
+	acc[i].addEventListener('click', function(e) {
+		e.preventDefault();
+		if (!(this.classList.contains('menu-acc__item_active'))) {
+			for (var i = 0; i < acc.length; i++) {
+				acc[i].classList.remove('menu-acc__item_active');
+			}
+			this.classList.add('menu-acc__item_active');
+		}
+		else if (this.classList.contains('menu-acc__item_active')) {
+			for (var i = 0; i < acc.length; i++) {
+				acc[i].classList.remove('menu-acc__item_active');
 			}
 		}
 	})
